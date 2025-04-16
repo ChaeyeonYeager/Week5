b474cb705a38a8124ce52eb60d5fcfb82409bc31
ChaeyeonYeager <chaegabond@naver.com>
2025-04-16 12:55:49 +0900

    Create gpio_m2_1.py

---
+++ m2/gpio_m2_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from gpiozero import LEDBoard, Button
+from signal import pause
+
+leds = LEDBoard(20, 21, 23, 24)
+button = Button(25, pull_up=True)
+
+# 버튼 상태가 변할 때마다 LED 켜고 끄기
+button.when_pressed = leds.on
+button.when_released = leds.off
+
+pause()  # 프로그램이 계속 실행되도록 대기
