5fbd802d62501e37c96df11f1d3eb6e91dd40449
ChaeyeonYeager <chaegabond@naver.com>
2025-04-22 12:11:39 +0900

    Update gpio_m2_2.py

--- m2/gpio_m2_2.py
+++ m2/gpio_m2_2.py
@@ -2,7 +2,7 @@
 from gpiozero import LED, Button
 from signal import pause
 
-leds = [LED(pin) for pin in [20, 21, 23, 24]]
+leds = [LED(pin) for pin in [8, 7, 16, 20]]
 button = Button(25, pull_up=True)
 
 button.when_pressed = lambda: [led.toggle() for led in leds]
